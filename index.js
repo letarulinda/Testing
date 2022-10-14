ef90c0fa46e95215ccce45878fe3ba86a307fb89
letarulinda <101931302+letarulinda@users.noreply.github.com>
2022-10-14 21:58:24 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function ("name") {
+  return name;
+}
+module.exports = name;
+const name = require('./name');
+
+test('string is at least character long and not long than 10 characters', () => {
+  expect Throws error if the condition is not met;
+});
+function reverse ("name") {
+  return reverse;
+}
